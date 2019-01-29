fae937b972844ef9d07775a8fee1ae692a8653c0
Sean Doyle <sdoylelambda@gmail.com>
2019-01-29 15:44:19 -0500

    Model controller and basic api search.

---
+++ iTunes App 1.29/iTunes App 1.29/SearchResultsController.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class SearchResultsController {
+    
+    let baseURL = "aaaaaaaaa"
+    
+    var searchResults: [SearchResult] =[]
+    
+    func performSearch(searchTerm: String, a resultType: ResultType, @Escaping error ) -> Void {
+        baseURL
+    }
+    
+}
